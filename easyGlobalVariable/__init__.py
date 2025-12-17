472fa8982feb6a48c55dab3075d3e193c5c7b1e0
rajeevkumarpv <138972685+rajeevkumarpv@users.noreply.github.com>
2025-12-17 09:00:40 +0530

    Delete easyGlobalVariable directory

--- easyGlobalVariable/__init__.py
+++
@@ -1,5 +0,0 @@
-import sys
-from .core import GlobalVariableManager
-
-# Replace the module object in sys.modules with an instance of GlobalVariableManager
-sys.modules[__name__] = GlobalVariableManager()
